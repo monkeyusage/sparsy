fbb5338f6156c0c241778a9330de06ce82f7031f
monkeyusage <monkeyusage@gmail.com>
2021-08-24 16:26:19 +0200

    compute maha distance

--- main.py
+++ main.py
@@ -3,35 +3,62 @@ import numpy as np
 
 from sys import argv
 
-try:
-    file = argv[1]
-except IndexError:
-    print("enter valid path for .csv analysis")
-    exit()
 
-data = pd.read_csv(file, sep="\t")
-data["division"] = data["division"].astype(np.uint64)
-data["year"] = data["year"].astype(np.uint16)
-data["nclass"] = data["nclass"].astype(np.uint8)
-data["firm"] = data["firm"].astype(np.uint16)
+def mahalanobis(x=np.ndarray, data=np.ndarray):
+    x_mu = x - np.mean(data)
+    cov = np.cov(data.T)
+    inv_covmat = np.linalg.inv(cov)
+    left = np.dot(x_mu, inv_covmat)
+    mahal = np.dot(left, x_mu.T)
+    return mahal.diagonal()
 
-data = data.sort_values("nclass")
-tclass_replacements = dict((k, v) for k, v in zip(data.nclass.unique(), range(data.nclass.nunique())))
-data["tclass"] = data.nclass.replace(tclass_replacements)
-tech = data["nclass"].nunique()
+def main():
+    try:
+        file = argv[1]
+    except IndexError:
+        print("enter valid path for .csv analysis")
+        exit()
 
-data = data.sort_values(["firm", "tclass"])
-tclass_max = data["tclass"].max()
+    data = pd.read_csv(file, sep="\t")
+    data["division"] = data["division"].astype(np.uint64)
+    data["year"] = data["year"].astype(np.uint16)
+    data["nclass"] = data["nclass"].astype(np.uint8)
+    data["firm"] = data["firm"].astype(np.uint16)
 
-subsh = pd.crosstab(data["firm"], data["tclass"]).astype(np.uint32)
-total = subsh.values.sum(axis=1)
-idx = subsh.index.values
-subsh = (subsh.values / total[:, None]) * 100
-subsh = pd.DataFrame({"firm":idx, **{f"subsh{idx}":subsh[:,idx] for idx in range(subsh.shape[1])}})
+    data = data.sort_values("nclass")
+    tclass_replacements = dict((k, v) for k, v in zip(data.nclass.unique(), range(data.nclass.nunique())))
+    data["tclass"] = data.nclass.replace(tclass_replacements)
 
-values = subsh.values[:,1:]
-copy = np.zeros_like(values)
+    data = data.sort_values(["firm", "tclass"])
 
-for i in range(len(values)):
-    for j in range(len(values)):
-        copy[i,j] = values[i,j] / (np.sqrt(values[i,i]) * np.sqrt(values[j,j]))
+    subsh = pd.crosstab(data["firm"], data["tclass"]).astype(np.uint32)
+    total = subsh.values.sum(axis=1)
+    subsh = (subsh.values / total[:, None]) * 100
+
+    del total
+
+    var = np.dot(subsh.T, subsh)
+    base_var = var.copy()
+
+    for i in range(var.shape[0]):
+        for j in range(var.shape[0]):
+            var[i,j] = var[i,j] / (np.sqrt(base_var[i,i]) * np.sqrt(base_var[j,j]))
+
+    norm_subsh = subsh.copy()
+    base_std = np.dot(subsh, subsh.T)
+    for i in range(base_std.shape[0]):
+        for j in range(var.shape[0]):
+            norm_subsh[i,j] = subsh[i,j] / np.sqrt(base_std[i,i])
+    
+    std = np.dot(norm_subsh, norm_subsh.T)
+    cov_std = np.dot(subsh, subsh.T)
+
+    mal_corr = np.dot(np.dot(norm_subsh, var),norm_subsh.T)
+    standard = mal_corr.copy()
+    covmal_corr = np.dot(np.dot(subsh, var), subsh.T)
+    cov_standard = covmal_corr.copy()
+    
+    import pdb;pdb.set_trace()
+
+if __name__ == "__main__":
+    main()
