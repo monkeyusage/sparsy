2cdc0b854f01b8fed3436542551ca575785e2f2f
monkey usage <monkeyusage@gmail.com>
2021-08-26 15:58:05 +0200

    stress test

--- main.py
+++ main.py
@@ -3,34 +3,53 @@ Parsy is a program that computes various distances between companies' caraterist
 """
 import pandas as pd
 import numpy as np
+from numba import njit
 from time import perf_counter
 
 from sys import argv
 
+def gen_data() -> np.ndarray:
+    return np.array([
+        np.random.randint(low=1, high=7000, size=15800000),
+        np.random.randint(low=1, high=6, size=15800000)
+    ]).T
+
+@njit
 def zero_diag(array: np.ndarray) -> np.ndarray:
+    assert array.shape[0] == array.shape[1], "array must be square matrix"
     for i in range(array.shape[0]):
         array[i,i] = 0
     return array
 
-def main():
-    try:
-        file = argv[1]
-    except IndexError:
-        print("enter valid path for .csv analysis")
-        exit()
-    
-    assert file.endswith(".tsv") or file.endswith(".dta"), "data file format should be either tsv or dta"
-    
-    # read in the data file
-    data = pd.read_csv(file, sep="\t") if file.endswith(".tsv") else pd.read_stata(file)
-    
-    del file
+@njit
+def tclass_corr(tech: int, var:np.ndarray) -> np.ndarray:
+    base_var = var.copy()
+    for i in range(tech):
+        for j in range(tech):
+            var[i,j] = var[i,j] / (np.sqrt(base_var[i,i]) * np.sqrt(base_var[j,j]))
+    return var
 
-    # set the appropriate data types
-    data["division"] = data["division"].astype(np.uint64)
-    data["year"] = data["year"].astype(np.uint16)
-    data["firm"] = data["firm"].astype(np.uint16)
-    data["nclass"] = data["nclass"].astype(np.uint8)
+@njit
+def firm_corr(num:int, tech:int, values:np.ndarray, base_std:np.ndarray) -> np.ndarray:
+    norm_values = values.copy()    
+    for j in range(tech):
+        for i in range(num):
+            norm_values[i,j] = values[i,j] / np.sqrt(base_std[i,i])
+    return norm_values
+
+def main(data: pd.DataFrame) -> None:
+    # try:
+    #     file = argv[1]
+    # except IndexError:
+    #     print("enter valid path for .csv analysis")
+    #     exit()
+    
+    # assert file.endswith(".tsv") or file.endswith(".dta"), "data file format should be either tsv or dta"
+    
+    # # read in the data file
+    # data = pd.read_csv(file, sep="\t") if file.endswith(".tsv") else pd.read_stata(file)
+
+    # del file
 
     # sort by nclass and create a new tclass independant of naming of nclass just in case
     data = data.sort_values("nclass")
@@ -41,32 +60,22 @@ def main():
 
     # crosstab on firm and class
     subsh = pd.crosstab(data["firm"], data["tclass"]).astype(np.uint16)
-    index = subsh.index.values.copy()
+    firms = subsh.index.values
     total = subsh.values.sum(axis=1)
     values = (subsh.values / total[:, None]) * 100
     tech = data["tclass"].nunique()
 
-    # del total, subsh, data
+    del total, subsh, data
 
     num = values.shape[0]
 
     # matrix of correlations between classes
     var = np.dot(values.T, values)
-    base_var = var.copy()
-
-    # var = tclass_corr(tech, var, base_var)
-    for i in range(tech):
-        for j in range(tech):
-            var[i,j] = var[i,j] / (np.sqrt(base_var[i,i]) * np.sqrt(base_var[j,j]))
+    var = tclass_corr(tech, var)
 
     # correlation between firms overs classes (n x n || 694x694)
     base_std = np.dot(values, values.T)
-    
-    # norm_values = firm_corr(num, tech, values, base_std)
-    norm_values = values.copy()
-    for j in range(tech):
-        for i in range(num):
-            norm_values[i,j] = values[i,j] / np.sqrt(base_std[i,i])
+    norm_values = firm_corr(num, tech, values, base_std)
     
     # generate standard measures
     std = (np.dot(norm_values, norm_values.T).round(decimals=2) * 100).astype(np.uint32)
@@ -81,21 +90,20 @@ def main():
     # then log each one
     results = dict((name,zero_diag(arr).sum(axis=1)) for name,arr in {"std":std, "mal":mal, "cov_std":cov_std, "cov_mal":cov_mal}.items())
 
-    # del values, norm_values, var, base_var
+    del values, norm_values, var
     assert len(set([arr.shape[0] for arr in results.values()])) == 1, "arrays are not all the same length"
 
     # df creation for further saving
-    df = pd.DataFrame(results)
+    df = pd.DataFrame({"firm":firms,**results})
 
     # saving into memory
     df.to_csv("data/spill_output_nolog.tsv", sep="\t", index=False)
-    pd.DataFrame(
-        data=np.log(df.values).astype(np.float16), columns=df.columns
-    ).to_csv("data/spill_output_log.tsv", sep="\t", index=False)
-    import pdb;pdb.set_trace()
+    df.iloc[:,1:] = np.log(df.drop("firm", axis=1).values, dtype=np.float32)
+    df.to_csv("data/spill_output_log.tsv", sep="\t", index=False)
 
 if __name__ == "__main__":
+    data = pd.DataFrame(data=gen_data(), columns=("firm", "nclass"))
     t0 = perf_counter()
-    main()
+    main(data)
     t1 = perf_counter()
     print(f"Elapsed time: {t1 - t0} seconds")
