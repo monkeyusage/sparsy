b9b97fec7487aa42c671cf9bdea587b02df4103f
monkeyusage <monkeyusage@gmail.com>
2021-09-01 16:36:59 +0200

    change installation numba every where incorporate year iteration

--- install.py
+++ install.py
@@ -1,11 +1,23 @@
 from os import system, path, mkdir
+from json import dump
 
 print("> Creating virtual environment for python and installing requirements")
 if not path.exists("venv"):
     system("python -m venv venv")
+else:
+    system(r".\venv\Scripts\activate && python -m pip uninstall -r requirements.txt")
 
 result = system(r".\venv\Scripts\activate && python -m pip install -r requirements.txt")
 
 if not path.exists("data"):
-    print("> did not find a data folder creating it, you should paste your data files there for convenience")
+    print("> did not find a data folder creating it")
     mkdir("data")
+    
+    config = {
+        "input_data":"",
+        "output_data":"",
+        "date_range":3
+    }
+
+    with open("data/config.json", "w") as config_file:
+        dump(config, config_file)
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 """
 Parsy is a program that computes various distances between companies' carateristics
 """
+from __future__ import annotations
+from typing import cast
 import pandas as pd
 import numpy as np
 from numba import njit
@@ -8,21 +10,22 @@ from time import perf_counter
 from json import load
 
 
-def gen_data() -> np.ndarray:
+def gen_data():
     return np.array([
         np.random.randint(low=1, high=500_000, size=15_800_000),
-        np.random.randint(low=1, high=6, size=15_800_000)
+        np.random.randint(low=1, high=1000, size=15_800_000),
+        np.random.randint(low=1970, high=2020, size=15_800_000)
     ]).T
 
 @njit
-def zero_diag(array: np.ndarray) -> np.ndarray:
+def zero_diag(array):
     assert array.shape[0] == array.shape[1], "array must be square matrix"
     for i in range(array.shape[0]):
         array[i,i] = 0
     return array
 
 @njit
-def tclass_corr(tech: int, var:np.ndarray) -> np.ndarray:
+def tclass_corr(tech: int, var):
     base_var = var.copy()
     for i in range(tech):
         for j in range(tech):
@@ -30,71 +33,94 @@ def tclass_corr(tech: int, var:np.ndarray) -> np.ndarray:
     return var
 
 @njit
-def firm_corr(num:int, tech:int, values:np.ndarray, base_std:np.ndarray) -> np.ndarray:
+def firm_corr(num:int, tech:int, values, base_std):
     norm_values = values.copy()    
     for j in range(tech):
         for i in range(num):
             norm_values[i,j] = values[i,j] / np.sqrt(base_std[i,i])
     return norm_values
 
-def main() -> None:
-    with open("data/config.json", "r") as config_file:
-        config = load(config_file)
+@njit
+def dot_zero(array_a, array_b):
+    dot_product = np.dot(array_a, array_b)
+    rounded = np.empty_like(dot_product)
+    np.round(dot_product, 2, rounded)
+    multiplied = np.multiply(rounded, 100)
+    zero_out = zero_diag(multiplied)
+    summed = np.sum(zero_out, axis=1)
+    logged = np.log(summed)
+    return logged
 
-    data = pd.read_csv(
-        "data/data.tsv",
-        sep="\t",
-        usecols=["firm", "nclass"],
-        dtype={
-            "firm":np.uint32,
-            "nclass":"category"
-        }
-    )
+@njit
+def compute(matrix, tech: int):
+    total = matrix.sum(axis=1)
 
-    # sort by nclass and create a new tclass independant of naming of nclass just in case
-    data = data.sort_values("nclass")
-    tclass_replacements = dict((k, v) for k, v in zip(data.nclass.unique(), range(data.nclass.nunique())))
-    data["tclass"] = data.nclass.replace(tclass_replacements)
-
-    # crosstab on firm and class
-    subsh = pd.crosstab(data["firm"], data["tclass"]).astype(np.uint32)
-    firms = subsh.index.values.copy()
-    total = subsh.values.sum(axis=1)
-    values = ((subsh.values / total[:, None]) * 100)
-    tech = data["tclass"].nunique()
-
-    del total, subsh, data
+    values = matrix
+    # values= (matrix / total[:, None]) * 100
+    for i in range(matrix.shape[0]):
+        for j in range(matrix.shape[1]):
+            values[i,j] = (matrix[i,j] / total[i]) * 100
 
     num = values.shape[0]
 
     # compute matrix of correlations between classes
-    var : np.ndarray = np.dot(values.T, values)
+    var = np.dot(values.T, values)
     var = tclass_corr(tech, var)
 
     # correlation between firms overs classes (n x n || 694x694)
-    base_std : np.ndarray = np.dot(values, values.T)
+    base_std = np.dot(values, values.T)
     norm_values = firm_corr(num, tech, values, base_std)
     
     # generate standard measures
-    std = (np.dot(norm_values, norm_values.T).round(2) * 100).astype(np.uint8)
-    cov_std = (np.dot(values, values.T).round(2) * 100).astype(np.uint32)
+    std = dot_zero(norm_values, norm_values.T)
+    cov_std = dot_zero(values, values.T)
 
     # generate MAHALANOBIS measure ==> gives n x n matrix
-    mal = (np.dot(np.dot(norm_values, var),norm_values.T).round(2) * 100).astype(np.uint8)
-    cov_mal = (np.dot(np.dot(values, var), values.T).round(2) * 100).astype(np.uint32)
+    mal = dot_zero(np.dot(norm_values, var),norm_values.T)
+    cov_mal = dot_zero(np.dot(values, var), values.T)
 
-    # flatten arrays and store them in dict for pandas purpose
-    results = dict((name,arr.flatten()) for name,arr in {"std":std, "mal":mal, "cov_std":cov_std, "cov_mal":cov_mal}.items())
+    return std, cov_std, mal, cov_mal
 
-    del values, norm_values, var
+def main() -> None:
+    with open("data/config.json", "r") as config_file:
+        config : dict[str, str | int] = load(config_file)
 
-    assert len(set([arr.shape[0] for arr in results.values()])) == 1, "arrays are not all the same length"
+    input_file : str = cast(str, config["input_data"])
+    data : pd.DataFrame =  pd.read_csv(
+        input_file,
+        sep="\t",
+        usecols=["firm", "nclass", "year"],
+        dtype={
+            "firm":np.uint32,
+            "nclass":"category",
+            "year":"category"
+        }
+    ) if not config["stress_test"] else \
+        pd.DataFrame(data=gen_data(), columns=("firm", "nclass", "year"))
+
+    # sort by nclass and create a new tclass independant of naming of nclass just in case
+    tclass_replacements = dict((k, v) for k, v in zip(data.nclass.unique(), range(data.nclass.nunique())))
+    data["tclass"] = data.nclass.replace(tclass_replacements)
+
+    # crosstab on firm and class
+    subsh = pd.crosstab(data["firm"], data["tclass"]).astype(np.float32)
+    firms = subsh.index.values.copy()
+
+    std, cov_std, mal, cov_mal = compute(matrix=subsh.values, tech=data["nclass"].nunique())
 
     # df creation for further saving
-    df = pd.DataFrame({"firm":firms,**results})
+    df = pd.DataFrame(
+        {
+            "year": year,
+            "firm":firms,
+            "std":std,
+            "cov_std":cov_std,
+            "mal":mal,
+            "cov_mal":cov_mal
+        }
+    )
     
     # saving into memory
-    df.iloc[:,1:] = np.log(df.drop("firm", axis=1).values, dtype=np.float32)
     df.to_csv("data/spill_output_log.tsv", sep="\t", index=False)
 
 if __name__ == "__main__":
