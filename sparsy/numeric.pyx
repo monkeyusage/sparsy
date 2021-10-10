d2cf17201f9f5b9adeac8249487cddfba015fb9e
MCSZN <monkeyusage@gmail.com>
2021-10-10 22:51:07 +0200

    woeifj;efpIOUWEHJFOPIWUHOIUswhefdeoiuvhadrvoiuaehrvoaeiurvhaoivuehvoieuhrvaloiuvhorviu

--- sparsy/numeric.pyx
+++ sparsy/numeric.pyx
@@ -3,37 +3,38 @@ cimport numpy as np
 cimport cython
 from cython.parallel import prange, parallel
 
-DTYPE = np.float
-ctypedef const np.float16 DTYPE_t
-
 @cython.boundscheck(False)
 @cython.wraparound(False)
-def dot_zero_cy1(np.ndarray[DTYPE_t, ndim=2] matrix) -> np.ndarray[DTYPE_t, 1]:
+@cython.nonecheck(False)
+def float[:] dot_zero_cy1(float[:, :] matrix) nogil:
     cdef int size = matrix.shape[0]
-    cdef np.ndarray[DTYPE_t, ndim=1] out = np.zeros(size, dtype=DTYPE)
+    cdef int idx = 0
+    cdef int idy = 0
+    cdef float[:] out = np.zeros(size, dtype=np.float32)
     cdef float total = 0.0
-    # with nogil:
-    for idx in range(size):
+    for idx in prange(size):
         total = 0.0
-        for idy in range(size):
+        idy = 0
+        while idy < size:
             if idx == idy:
                 total  += 0
             else:
                 total += np.dot(matrix[idx], matrix[idy])
+            idy += 1
         out[idx] = total
     return out
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-def dot_zero_cy2(np.ndarray[DTYPE_t, ndim=2] matrix) -> np.ndarray[DTYPE_t, 1]:
-    cdef int K = matrix.shape[0]
-    cdef int J = matrix.shape[1]
-    cdef np.ndarray flat = matrix.flatten()
-    cdef np.ndarray out = np.zeros(K)
-    cdef np.ndarray x
+@cython.nonecheck(False)
+def float[:] dot_zero_cy2(float[:, :] matrix) nogil:
+    cdef Py_ssize_t  K = matrix.shape[0]
+    cdef Py_ssize_t  J = matrix.shape[1]
+    cdef float[:] flat = matrix.flatten()
+    cdef float[:] out = np.zeros(K)
+    cdef float[:] x
     cdef int i
     cdef float total
-    # with nogil:
     for k in prange(K):
         x = matrix[k, :]
         i = 0
@@ -50,12 +51,12 @@ def dot_zero_cy2(np.ndarray[DTYPE_t, ndim=2] matrix) -> np.ndarray[DTYPE_t, 1]:
 
 @cython.boundscheck(False)
 @cython.wraparound(False)
-def dot_zero_cy3(np.ndarray matrix) -> np.ndarray[DTYPE_t, 1]:
-    cdef int K = matrix.shape[0]
-    cdef int J = matrix.shape[1]
-    cdef int I = matrix.shape[0]
-    cdef np.ndarray out = np.zeros(K)
-    # with nogil:
+@cython.nonecheck(False)
+def float[:] dot_zero_cy3(float[:, :] matrix) nogil:
+    cdef Py_ssize_t  K = matrix.shape[0]
+    cdef Py_ssize_t  J = matrix.shape[1]
+    cdef Py_ssize_t  I = matrix.shape[0]
+    cdef float[:] out = np.zeros(K)
     for k in prange(K):
         total = 0
         for i in range(I):
