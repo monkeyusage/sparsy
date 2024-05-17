6410beac16bbbfc0fe0b9641ff0198c36973d95e
marc <monkeyusage@gmail.com>
2024-05-17 14:22:39 +0200

    update benchmark

--- evol/benchmark.py
+++ evol/benchmark.py
@@ -13,8 +13,10 @@ def main() -> None:
     #assert dot_naive == dot_unrolled, f'{dot_naive=} != {dot_unrolled=}'
     #print('naive: ', timeit(lambda: naive.sparsy(df.values), number=10))
     #print('unrolled: ', timeit(lambda: unrolled.sparsy(df.values), number=10))
-    print('fast: ', timeit(lambda: fast.sparsy(df.values), number=1))
-    print('faster: ', timeit(lambda: faster.sparsy(df.values), number=1))
+    #fast.sparsy(df.values[:10, :])
+    #faster.sparsy(df.values[:10, :])
+    #print('fast: ', timeit(lambda: fast.sparsy(df.values), number=1))
+    #print('faster: ', timeit(lambda: faster.sparsy(df.values), number=1))
 
     
 
